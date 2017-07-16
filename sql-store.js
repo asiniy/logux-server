49f63a95fdfa6330507b2821add76b6561151ede
Andrey Sitnik <andrey@sitnik.ru>
2017-07-16 17:29:35 -0500

    Fix class JSDoc position

--- sql-store.js
+++ sql-store.js
@@ -1,22 +1,23 @@
 'use strict'
 
+/**
+ * Store Logux log in SQL databases.
+ *
+ * @param {object} config Knex config for database connection.
+ * @param {string} [prefix="logux_"] Name prefix for log tables.
+ *
+ * @example
+ * const SqlStore = require('logux/sql-store')
+ *
+ * const log = new Log({
+ *   store: new SqlStore({
+ *     client: 'pg',
+ *     connection: process.env.PG_CONNECTION_STRING
+ *   }),
+ *   nodeId
+ * })
+ */
 class SqlStore {
-  /**
-   * Store Logux log in SQL databases.
-   *
-   * @param {object} config Knex config for database connection.
-   * @param {string} [prefix="logux_"] Name prefix for log tables.
-   *
-   * @example
-   * const SqlStore = require('logux/sql-store')
-   * consnt log = new Log({
-   *   store: new SqlStore({
-   *     client: 'pg',
-   *     connection: process.env.PG_CONNECTION_STRING
-   *   }),
-   *   nodeId
-   * })
-   */
   constructor (config, prefix) {
     this.config = config
     this.prefix = prefix || 'logux_'
